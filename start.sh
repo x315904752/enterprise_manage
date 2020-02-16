8983ec97cdac051a56d2e60cf8406ba215784793
徐磊 <x@iparp.com>
2020-02-16 17:22:00 +0800

    修复依赖，完成启动文件

--- start.sh
+++ start.sh
@@ -5,7 +5,8 @@ mkdir /opt/proj/script
 touch /opt/proj/script/uwsgi.pid
 touch /opt/proj/script/uwsgi.sock
 touch /var/log/enterprise_manage/uwsgi.log
-pip install -r /opt/enterprise_manage/requirements.txt
+pip3 install -r /opt/enterprise_manage/requirements.txt
 python /opt/enterprise_manage/manage.py makemigrations
 python /opt/enterprise_manage/manage.py migrate
 uwsgi --ini /www/enterprise_manage/uwsgi.ini
+tail -f /var/log/business_monitor/uwsgi.log
